8c5a3b122c270c3a509c1de4f373d61f779fa02c
longchao.chen <longchao.chen@24tidy.com>
2015-09-17 10:39:18 +0800

    Initial Commit

---
+++ Thousand Eyes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Thousand Eyes
+//
+//  Created by longchao.chen on 15/9/17.
+//  Copyright (c) 2015年 longchao.chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Thousand Eyes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Thousand Eyes
+//
+//  Created by longchao.chen on 15/9/17.
+//  Copyright (c) 2015年 longchao.chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
